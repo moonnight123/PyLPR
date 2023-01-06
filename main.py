a727bf61a21461b4a9360bafcaf977860b6d5e39
moonnight123 <79900521+moonnight123@users.noreply.github.com>
2023-01-06 22:07:18 +0800

    创建设计了窗口

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import cv2
 from matplotlib import pyplot as plt
 import os
 import numpy as np
-
+from tkinter import *
+from tkinter import filedialog
+from PIL import Image
 
 # plt显示彩色图片
 def plt_show0(img):
@@ -23,94 +25,6 @@ def gray_guss(image):
     gray_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     return gray_image
 
-
-origin_image = cv2.imread('Car.jpg')
-image = origin_image.copy()
-gray_image = gray_guss(image)
-
-Sobel_x = cv2.Sobel(gray_image, cv2.CV_16S, 1, 0)
-absX = cv2.convertScaleAbs(Sobel_x)
-image = absX
-
-ret, image = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)
-plt_show(image)
-kernelX = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 10))
-image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernelX,iterations = 1)
-
-kernelX = cv2.getStructuringElement(cv2.MORPH_RECT, (50, 1))
-kernelY = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 20))
-
-image = cv2.dilate(image, kernelX)
-image = cv2.erode(image, kernelX)
-
-image = cv2.erode(image, kernelY)
-image = cv2.dilate(image, kernelY)
-
-image = cv2.medianBlur(image, 21)
-
-contours, hierarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-for item in contours:
-    rect = cv2.boundingRect(item)
-    x = rect[0]
-    y = rect[1]
-    weight = rect[2]
-    height = rect[3]
-    if (weight > (height * 3.5)) and (weight < (height * 4)):
-        image = origin_image[y:y + height, x:x + weight]
-
-gray_image = gray_guss(image)
-# 图像阈值化操作——获得二值化图
-ret, image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_OTSU)
-plt_show(image)
-
-#膨胀操作
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
-image = cv2.dilate(image, kernel)
-plt_show(image)
-
-contours, hierarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-words = []
-word_images = []
-#对所有轮廓逐一操作
-for item in contours:
-    word = []
-    rect = cv2.boundingRect(item)
-    x = rect[0]
-    y = rect[1]
-    weight = rect[2]
-    height = rect[3]
-    word.append(x)
-    word.append(y)
-    word.append(weight)
-    word.append(height)
-    words.append(word)
-# 排序，车牌号有顺序。words是一个嵌套列表
-words = sorted(words,key=lambda s:s[0],reverse=False)
-i = 0
-#word中存放轮廓的起始点和宽高
-for word in words:
-    # 筛选字符的轮廓
-    if (word[3] > (word[2] * 1.5)) and (word[3] < (word[2] * 3.5)) and (word[2] > 25):
-        i = i+1
-        splite_image = image[word[1]:word[1] + word[3], word[0]:word[0] + word[2]]
-        word_images.append(splite_image)
-        print(i)
-print(words)
-
-for i,j in enumerate(word_images):
-    plt.subplot(1,7,i+1)
-    plt.imshow(word_images[i],cmap='gray')
-plt.show()
-
-
-#模版匹配
-# 准备模板(template[0-9]为数字模板；)
-template = ['0','1','2','3','4','5','6','7','8','9',
-            'A','B','C','D','E','F','G','H','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z',
-            '藏','川','鄂','甘','赣','贵','桂','黑','沪','吉','冀','津','晋','京','辽','鲁','蒙','闽','宁',
-            '青','琼','陕','苏','皖','湘','新','渝','豫','粤','云','浙']
-
 # 读取一个文件夹下的所有图片，输入参数是文件名，返回模板文件地址列表
 def read_directory(directory_name):
     referImg_list = []
@@ -126,8 +40,6 @@ def get_chinese_words_list():
         c_word = read_directory('./refer1/'+ template[i])
         chinese_words_list.append(c_word)
     return chinese_words_list
-chinese_words_list = get_chinese_words_list()
-
 
 # 获得英文模板列表（只匹配车牌的第二个字符）
 def get_eng_words_list():
@@ -136,8 +48,6 @@ def get_eng_words_list():
         e_word = read_directory('./refer1/'+ template[i])
         eng_words_list.append(e_word)
     return eng_words_list
-eng_words_list = get_eng_words_list()
-
 
 # 获得英文和数字模板列表（匹配车牌后面的字符）
 def get_eng_num_words_list():
@@ -146,8 +56,6 @@ def get_eng_num_words_list():
         word = read_directory('./refer1/'+ template[i])
         eng_num_words_list.append(word)
     return eng_num_words_list
-eng_num_words_list = get_eng_num_words_list()
-
 
 # 读取一个模板地址与图片进行匹配，返回得分
 def template_score(template,image):
@@ -213,6 +121,112 @@ def template_matching(word_images):
             results.append(r)
             continue
     return results
+
+
+win1 = Tk()#常见窗口对象
+win1.title('车牌识别')#添加窗体名称
+win1.geometry('670x470')#设置窗体大小
+export_filename = ""
+def jpg_to_gif():
+    global im1
+    import_filename = filedialog.askopenfilename()
+    if import_filename.endswith(".jpg"):
+        im1 = Image.open(import_filename)
+        export_filename = filedialog.asksaveasfilename(defaultextension=".gif")
+        im1.save(export_filename)
+    else:
+        Label_2 = Label(win1, text="Error!", width=20,fg="red", font=("bold", 15))
+        Label_2.place(x=80, y=280)
+showImgButton = Button(win1,text = "选择图片",command=jpg_to_gif)
+showImgButton.pack();
+print(export_filename)
+win1.mainloop()#执行窗体
+
+origin_image = cv2.imread('Car.jpg')
+image = origin_image.copy()
+gray_image = gray_guss(image)
+
+Sobel_x = cv2.Sobel(gray_image, cv2.CV_16S, 1, 0)
+absX = cv2.convertScaleAbs(Sobel_x)
+image = absX
+
+ret, image = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)
+kernelX = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 10))
+image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernelX,iterations = 1)
+
+kernelX = cv2.getStructuringElement(cv2.MORPH_RECT, (50, 1))
+kernelY = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 20))
+
+image = cv2.dilate(image, kernelX)
+image = cv2.erode(image, kernelX)
+
+image = cv2.erode(image, kernelY)
+image = cv2.dilate(image, kernelY)
+
+image = cv2.medianBlur(image, 21)
+
+contours, hierarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+for item in contours:
+    rect = cv2.boundingRect(item)
+    x = rect[0]
+    y = rect[1]
+    weight = rect[2]
+    height = rect[3]
+    if (weight > (height * 3.5)) and (weight < (height * 4)):
+        image = origin_image[y:y + height, x:x + weight]
+
+gray_image = gray_guss(image)
+# 图像阈值化操作——获得二值化图
+ret, image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_OTSU)
+
+#膨胀操作
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+image = cv2.dilate(image, kernel)
+
+contours, hierarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+words = []
+word_images = []
+#对所有轮廓逐一操作
+for item in contours:
+    word = []
+    rect = cv2.boundingRect(item)
+    x = rect[0]
+    y = rect[1]
+    weight = rect[2]
+    height = rect[3]
+    word.append(x)
+    word.append(y)
+    word.append(weight)
+    word.append(height)
+    words.append(word)
+# 排序，车牌号有顺序。words是一个嵌套列表
+words = sorted(words,key=lambda s:s[0],reverse=False)
+i = 0
+#word中存放轮廓的起始点和宽高
+for word in words:
+    # 筛选字符的轮廓
+    if (word[3] > (word[2] * 1.5)) and (word[3] < (word[2] * 3.5)) and (word[2] > 25):
+        i = i+1
+        splite_image = image[word[1]:word[1] + word[3], word[0]:word[0] + word[2]]
+        word_images.append(splite_image)
+
+
+for i,j in enumerate(word_images):
+    plt.subplot(1,7,i+1)
+    plt.imshow(word_images[i],cmap='gray')
+
+
+#模版匹配
+# 准备模板(template[0-9]为数字模板；)
+template = ['0','1','2','3','4','5','6','7','8','9',
+            'A','B','C','D','E','F','G','H','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z',
+            '藏','川','鄂','甘','赣','贵','桂','黑','沪','吉','冀','津','晋','京','辽','鲁','蒙','闽','宁',
+            '青','琼','陕','苏','皖','湘','新','渝','豫','粤','云','浙']
+
+chinese_words_list = get_chinese_words_list()
+eng_words_list = get_eng_words_list()
+eng_num_words_list = get_eng_num_words_list()
 
 
 word_images_ = word_images.copy()
